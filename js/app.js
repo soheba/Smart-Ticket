fc8858fb5bbd7fa38cf295ad9b9b00ab0300ebab
soheba <sohebaakter9@gmail.com>
2024-02-18 23:40:32 +0600

    js added, some code and function added

---
+++ js/app.js
@@ -0,0 +1,67 @@
+//GLOBAL VARIABLES
+let numberOfSeats = 40;
+let totalSeats = 0;
+let grandTotal = 0;
+
+//SELECTORS
+let numberOfSeatsContainer = document.getElementById("number_of_seats");
+let takeSeatBtn = document.getElementById("take_seat");
+let seats = document.querySelectorAll("#seat");
+let seatList = document.getElementById("seat_list");
+let totalPriceSpan = document.getElementById("total_price");
+
+//
+console.log(seats);
+//adding click event to all the seats
+let seatsArr = Array.from(seats);
+
+for (let i = 0; i < seatsArr.length; i++) {
+  seatsArr[i].addEventListener("click", () => {
+    if (seatsArr[i].classList.contains("bg-green-600")) {
+      //do nothing
+    } else {
+      numberOfSeats--;
+      totalSeats++;
+      numberOfSeatsContainer.innerText = numberOfSeats + " seats";
+      seatsArr[i].classList.add("bg-green-600");
+
+      let newrow = document.createElement("tr");
+
+      let seatName = document.createElement("td");
+      seatName.innerText = seatsArr[i].innerText;
+
+      let seatClass = document.createElement("td");
+      seatClass.innerText = "Economy";
+      let seatPrice = document.createElement("td");
+      seatPrice.innerText = "550";
+      newrow.appendChild(seatName);
+      newrow.appendChild(seatClass);
+      newrow.appendChild(seatPrice);
+
+      seatList.appendChild(newrow);
+
+      //totalprice update
+      grandTotal = calculateTotal(totalSeats, 550);
+      totalPriceSpan.innerText = grandTotal;
+
+      console.log(totalSeats);
+    }
+  });
+}
+
+numberOfSeatsContainer.innerText = numberOfSeats + " seats";
+
+takeSeatBtn.addEventListener("click", () => {
+  numberOfSeats--;
+  totalSeats++;
+  numberOfSeatsContainer.innerText = numberOfSeats + " seats";
+  console.log(numberOfSeats);
+});
+
+//functions
+
+function calculateTotal(totalNumberSeats, ticketprice) {
+  return ticketprice * totalNumberSeats;
+}
+//valid invalid check in next buton
+//let pasNAme.value == "" || phon === "" || email == "" ==> invalid
